05b9d51b2d9b040043b6190616eb8ce854c1ff7c
廖燊 <liaoshen@mama.cn>
2022-06-24 14:55:27 +0800

    为组件新增加方法，新增加类

---
+++ MamaChainProgram/Classes/Category/Person.h
@@ -0,0 +1,16 @@
+//
+//  Person.h
+//  MamaChainProgram
+//
+//  Created by liaoshen on 2022/6/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MamaChainProgram/Classes/Category/Person.m
@@ -0,0 +1,14 @@
+//
+//  Person.m
+//  MamaChainProgram
+//
+//  Created by liaoshen on 2022/6/24.
+//
+
+#import "Person.h"
+
+@implementation Person
++ (void)eat{
+    NSLog(@"开始吃饭啦");
+}
+@end
